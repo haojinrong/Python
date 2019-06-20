4773e8e81a775351dd4fd89bd4ad267ec5feb0fe
haojinrong <51935673+haojinrong@users.noreply.github.com>
2019-06-20 11:38:09 +0800

    Add files via upload

--- hjr.py
+++ hjr.py
@@ -1,26 +1,10 @@
-def function_name(panams):
-    res = re.search("(.jpg|.JPG|.gif|.GIF|.bmp|.BMP|.png|.PNG|.jpeg|.JPEG)$",url)
 import urllib.request
-import urllib.parse
-data = urllib.parse.urlencode({'wd':'学生'},encoding='utf8')
-print(data)
-ur1='http://www.baidu.com/s?'+data
-print(ur1)
-response = urllib.request.urlopen(url)
-HTML = response.read().decode('utf8')
-print(HTML)
 
-
-export default{
-    data(){
-        return{
-            id:''
-        }
-        mounted : function(){
-            this.id = this.route.params.tastId;
-    }
-}
-data = bytes(urllib.parse.urlencode({'pw':'456','un':'756'}),enconding='utf8')
-url = 'http://httpbin.org/post'
-response = urllib.request.urlopen(url,data=data)
-print(result)
+urls = ('http://httpbin.org/get'，'http://www.baidu.com/get'，'http://www.souhu.com')
+for url in urls:
+    response = urllib.request.urlopen(url)
+    print(response.status)
+    if response.status ==200:
+        print('%s 该网站继续爬')
+    else：
+        print('%s错误')
